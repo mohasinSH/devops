f137fd754f7f5ecbfd7fb71904026e591fbbd7ba
mohasinSH <ismail4111969@gmail.com>
2024-08-09 01:38:08 +0530

    updated

--- app.js
+++ app.js
@@ -1,12 +1,13 @@
 const http = require('express')()
+const express = require('express');
 const mongoose = require('mongoose')
 const cors = require('cors');
 const port =  8000
 const password = 'WAbF3wEhqHA4YjGA'
 const uri = `mongodb+srv://aws-user:${password}@aws-test-cluster.ungtg.mongodb.net/mern`
 mongoose.connect(uri)
-app.use(cors());
-app.use(express.json());
+http.use(cors());
+http.use(express.json());
 
 http.listen(port,()=>{
 console.log(`the server is litsening to port ${port}`)
